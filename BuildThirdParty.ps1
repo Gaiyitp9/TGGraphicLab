574813baf5c4b1d6d6a133e7e2c51f20e2240ac0
Gaiyitp9 <932404058@qq.com>
2024-12-20 18:24:03 +0800

    整理构建代码

--- BuildThirdParty.ps1
+++ BuildThirdParty.ps1
@@ -1,4 +1,5 @@
 # 构建TGGraphicLab依赖的三方库
+
 # 配置构建目录和安装目录
 $sourceDir = "$PWD\ThirdParty"
 $buildDir = "$PWD\build"
@@ -8,39 +9,48 @@ Write-Host "Source directory: $sourceDir"
 Write-Host "Build directory: $buildDir"
 Write-Host "Install directory: $installDir"
 
-# CMake 配置参数
+# 确保构建目录存在
+if (-Not (Test-Path -Path $buildDir))
+{
+   New-Item -ItemType Directory -Path $buildDir
+}
+
+# CMake配置参数
 $generator = "Visual Studio 17 2022"
-$buildType = "Debug" # 构建类型（Debug, Release, RelWithDebInfo, MinSizeRel）
 $cmakeOptions = @(
-   "-DCMAKE_BUILD_TYPE=$buildType",
    "-DCMAKE_INSTALL_PREFIX=$installDir"
    "-DCMAKE_VERBOSE_MAKEFILE=OFF"
    "-DSPDLOG_USE_STD_FORMAT:BOOL=ON"
    "-DSPDLOG_INSTALL:BOOL=ON"
 )
 
-# 确保构建目录存在
-if (-Not (Test-Path -Path $buildDir)) 
+function BuildLibrary
 {
-   New-Item -ItemType Directory -Path $buildDir
+   param(
+      [string]$buildType
+   )
+
+   # 运行 CMake 配置命令
+   Write-Host "Configuring the $buildType project with CMake..."
+   $cmakeCmd = "cmake -DCMAKE_BUILD_TYPE=$buildType $($cmakeOptions -join ' ') -G '$generator'" +
+      "-S $sourceDir -B $buildDir"
+   Write-Host "Running: $cmakeCmd"
+   Invoke-Expression $cmakeCmd
+   # 运行 CMake 构建命令
+   Write-Host "Building the project..."
+   $buildCmd = "cmake --build $buildDir --config $buildType"
+   Write-Host "Running: $buildCmd"
+   Invoke-Expression $buildCmd
+   # 安装
+   Write-Host "Installing the project to $installDir..."
+   $installCmd = "cmake --install $buildDir --config $buildType"
+   Write-Host "Running: $installCmd"
+   Invoke-Expression $installCmd
 }
 
-# 运行 CMake 配置命令
-Write-Host "Configuring the project with CMake..."
-$cmakeCmd = "cmake $($cmakeOptions -join ' ') -G '$generator' -S $sourceDir -B $buildDir"
-Write-Host "Running: $cmakeCmd"
-Invoke-Expression $cmakeCmd
+# 构建Debug版本
+BuildLibrary "Debug"
+# 构建Release版本
+BuildLibrary "Release"
 
-# 运行 CMake 构建命令
-Write-Host "Building the project..."
-$buildCmd = "cmake --build $buildDir --config $buildType"
-Write-Host "Running: $buildCmd"
-Invoke-Expression $buildCmd
-
-# 安装
-Write-Host "Installing the project to $installDir..."
-$installCmd = "cmake --install $buildDir --config $buildType"
-Write-Host "Running: $installCmd"
-Invoke-Expression $installCmd
-
-Write-Host "Build and configuration complete!"
+Write-Host "Configuration and build complete!"
